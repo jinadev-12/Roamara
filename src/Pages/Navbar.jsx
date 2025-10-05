fc8172f774f159913b880a6b2573b905afbf4403
Jinadev <jinad3v@gmail.com>
2025-10-05 22:20:44 +0530

    navbar touch-2

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -5,17 +5,17 @@ function Navbar() {
     // main
     <div className="fixed w-[100%] z-20">
       {/* Container */}
-      <div className=" flex justify-between items-center  m-auto max-w-[1350px] font-secondary pt-9 pb-2 bg-transparent">
+      <div className=" flex justify-between w-[90%] items-center  m-auto max-w-[1350px] font-secondary pt-9 pb-2 bg-transparent">
         {/* logo and links */}
-        <div className="flex gap-4 text-black">
+        <div className="flex gap-5 text-black">
           {/* logo */}
-          <div className="font-primary flex gap-2 ">
+          <div className="font-primary flex gap-5 ">
             <h1 className="text-lg">RM</h1>
             <div class="w-px h-6 bg-gray-400"></div>
           </div>
 
           {/* links */}
-          <div>
+          <div className='hidden lg:block'>
             <ul className="flex gap-5 font-medium">
               <li>
                 <a href="#ranch-life">Ranch life</a>
@@ -36,13 +36,15 @@ function Navbar() {
           </div>
         </div>
         {/* menu and book a vist buttons */}
-        <div className="">
+        <div className="flex items-center gap-5">
           {/* book button */}
-          <div className=""></div>
-          {/* menu-button */}
-          <div className="group text-white bg-black inline-block py-3 px-6 rounded-lg font-medium cursor-pointer">
+          <div className="hidden md:inline-block group text-white bg-black  py-3 px-6 rounded-lg font-medium cursor-pointer">
             Plan a Trip
             <i className="ri-sparkling-line ml-3  transition-transform duration-300 transform group-hover:rotate-[15deg]"></i>
+          </div>
+          {/* menu-button */}
+          <div className='lg:hidden'>
+            <i class="ri-menu-line text-2xl md:text-3xl"></i>
           </div>
         </div>
       </div>
