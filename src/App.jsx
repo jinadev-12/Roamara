827217a643a0f5305ea843e7c400ab326a01857c
akhilsm <primeorwott@gmail.com>
2025-11-03 13:56:51 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
-import React from 'react'
-import './index.css'
-import Navbar from './Pages/Navbar';
-import Hero from './Pages/Hero';
-import Footer from './Pages/Footer';
-import Events from './Pages/Events';
-import Beauty from './Pages/Beauty';
-import Direction from './Pages/Direction';
-import RanchLife from './Pages/RanchLife';
-import Natural from './Pages/Natural';
-
-
-
-
+import React from "react";
+import "./index.css";
+import Navbar from "./Pages/Navbar";
+import Hero from "./Pages/Hero";
+import Footer from "./Pages/Footer";
+import Events from "./Pages/Events";
+import Beauty from "./Pages/Beauty";
+import Direction from "./Pages/Direction";
+import RanchLife from "./Pages/RanchLife";
+import Natural from "./Pages/Natural";
 
 function App() {
   return (
     <div className="bg-bgcolor">
-      <div className='relative'>
-      <Navbar />
-      <Hero />
+      <div className="relative">
+        <Navbar />
+        <Hero />
       </div>
       <RanchLife />
       <Natural />
       <Events />
       <Direction />
-      <Beauty/>
+      <Beauty />
       <Footer />
     </div>
   );
 }
 
-export default App
+export default App;
